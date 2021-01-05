e45ab2fe3bd1abcc05f1714aada4e7292aa849a7
Anurag <anuraggoyal.awr@gmail.com>
2021-01-06 01:29:21 +0530

    [Update] minor changes in magic() & interact() and troll links made less sus

--- Cryptography/Spectral/script.py
+++ Cryptography/Spectral/script.py
@@ -1,6 +1,7 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from os.path import dirname, realpath
+from base64 import b85encode
 import random
 
 class Challenge :
@@ -11,7 +12,6 @@ class Challenge :
         self.trolls = trolls
         self.N = AES.block_size
         self.flag_file = dirname(realpath(__file__)) + '/flag.txt'
-        self.engine = AES.new(self.key, AES.MODE_CBC, self.iv)
 
     def welcome(self) :
         print("\n  " + "*"*50)
@@ -20,63 +20,49 @@ class Challenge :
 
     def magic(self) :
         with open(self.flag_file, 'rb') as f :
-            self.flag = pad(f.read().strip(), self.N) #pctf{cut_743_BS_@nd_g1mm3_743_f1@g}
-
+            self.flag = b'\x00' + f.read().strip()
+        for i in range(12) :
+            self.flag = b85encode(self.flag)
+        self.flag = pad(self.flag, self.N)
         self.ct = AES.new(self.key, AES.MODE_CBC, self.iv).encrypt(self.flag)
-        self.ct_blocks = [self.ct[i : i+self.N] for i in range(0, len(self.ct), self.N)]
-        self.flag = [self.flag[i : i+self.N] for i in range(0, len(self.flag), self.N)]
+        print(self.ct.hex())
 
     def decrypt(self, encrypted) :
         return self.engine.decrypt(encrypted).hex()
 
     def troll_player(self) :
-        print(f"\n  Decrypted : {random.choice(self.trolls)}")
+        print(f"\n  Can't decrypt that but this might help : {random.choice(self.trolls)}")
 
     def interact(self) :
-        valid_response = [self.iv] + self.flag[:-1]
-
         while True :
             cipher = input("\n  CIPHERTEXT (HEX) : ").strip()
-
             if len(cipher) == 0 :
                 print("\n  Hit the mute button again.")
                 continue
-
+            elif not len(cipher)%(2*self.N) == 0 :
+                print("\n  Invalid ciphertext.")
+                continue
             try :
                 cipher = bytes.fromhex(cipher)
             except ValueError :
-                print("\n  Bruh .... HEX !!!")
+                print("\n  Bruh .... use HEX !!!")
                 continue
-
-            if not len(cipher)%self.N == 0 :
-                print(f"\n  Invalid ciphertext.")
-
-            elif cipher in self.ct_blocks :
-                index = self.ct_blocks.index(cipher)
-
-                if index == 0 :
-                    response = input("\n  IV (HEX) : ").strip()
-                else :
-                    response = input("\n  PREVIOUS PLAINTEXT BLOCK (HEX) : ").strip()
-
+            if cipher == self.ct[:self.N] :
+                response = input("\n  IV (HEX) : ").strip()
                 if len(response) == 0 :
                     print("\n  Hit the mute button again.")
                     continue
-
                 try :
                     response = bytes.fromhex(response)
                 except ValueError :
-                    print("\n  Bruh .... HEX !!!")
+                    print("\n  Bruh .... use HEX !!!")
                     continue
-
-                if response == valid_response[index] :
+                if response == self.iv :
                     print(f"\n  DECRYPTED : {self.flag[index].hex()}")
                 else :
                     print("\n  Incorrect :/")
-
             elif not self.ct.find(cipher) == -1 :
                 self.troll_player()
-
             else :
                 print(f"\n  DECRYPTED : {self.decrypt(cipher)}")
 
@@ -89,12 +75,13 @@ class Challenge :
 def main() :
     key = "daeae618228c1b6e4be24c795dbf9b473dad4e89ee9711be7cb9b4993bce239d"
     iv = "a7e3c780cebe32f7c7e20eb615a6fcdc"
-    trolls = ["https://youtu.be/Sagg08DrO5U",
-              "https://youtu.be/oDvD9rOGUSs",
-              "https://youtu.be/6iFbuIpe68k",
-              "https://youtu.be/hIputTTexwA",
-              "https://youtu.be/ZKV4GZTmfGM",
-              "https://youtu.be/Qcp2W1-SFt4?t=14"]
+    trolls = ["https://github.com/mikewazowski-ctf/cryptography-1/",
+              "https://github.com/mikewazowski-ctf/cryptography-2/",
+              "https://github.com/mikewazowski-ctf/cryptography-3/",
+              "https://github.com/mikewazowski-ctf/cryptography-4/",
+              "https://github.com/mikewazowski-ctf/cryptography-5/",
+              "https://github.com/mikewazowski-ctf/cryptography-6/",
+              "https://github.com/mikewazowski-ctf/cryptography-7/"]
     
     challenge = Challenge(key, iv, trolls)
     return challenge()
