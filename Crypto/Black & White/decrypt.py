6ce37928bdc9156feefb1bb60632d2d5f6466f45
Akarsh Malik <malikakarsh@gmail.com>
2021-01-14 09:15:34 +0530

    2nd question of PCTF

--- Crypto/Black & White/decrypt.py
+++
@@ -1,3 +0,0 @@
-from stegano import lsb
-clear_message = lsb.reveal("secret.png")
-print(clear_message)
--- Crypto/Black & White/flip.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# Open an already existing image
-imageObject = Image.open("full_b.png")
-
-# Do a flip of left and right
-secret_inv = imageObject.transpose(Image.FLIP_LEFT_RIGHT)
-#secret_inv = imageObject.transpose(Image.FLIP_TOP_BOTTOM)
-secret_inv.save("flip_b.png")
